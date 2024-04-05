309c582a06ec27d57b4a66b18f9b8d23b38b288f
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-04-05 19:50:02 +0500

    Completed Hero component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => {
   return (
-    <div>
-      GetStarted
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-1 cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium leading-[23px] text-[18px] mr-2 ">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            alt="arrow"
+            className="w-[23px] object-contain h-[23px]"
+          />
+        </div>
+        <p className="font-poppins font-medium leading-[23px] text-[18px] ">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GetStarted
+export default GetStarted;

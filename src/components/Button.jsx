cf6ebe65d176bab14e046e92a280ade7b430009e
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-04-07 03:04:05 +0500

    Completed CardDeal component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles }) => {
   return (
     <button
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] ${styles} text-primary outline-none`}
+      className={` rounded-[10px] py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] ${styles} text-primary outline-none`}
       type="button"
     >
       Get Started

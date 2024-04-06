fd48b8fca2e9c08cdccaa23852c596f007cf2432
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-04-07 02:12:51 +0500

    Completed Business component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>
-      Button
-    </div>
-  )
-}
+    <button
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] ${styles} text-primary outline-none`}
+      type="button"
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
